562343b5636636922854f5256783bbcb1388db2e
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-04 10:33:21 -0600

    Add register new team functionality

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,16 @@ class TeamsController < ApplicationController
   def new
 
   end
+
+  def create
+    competition = Competition.find(params[:competition_id])
+    team = Team.create!(team_params)
+    competition.teams << team
+
+    redirect_to "/competitions/#{competition.id}"
+  end
+
+  def team_params
+    params.permit(:nickname, :hometown)
+  end
 end
