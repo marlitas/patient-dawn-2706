84071e757f576125ef1835b516d91906695ff68d
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-04 09:58:28 -0600

    Add associated teams to show

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -4,6 +4,21 @@ RSpec.describe 'Competition Show' do
   before(:each) do
     @competition1 = Competition.create!(name: 'Big Bowl', location: 'Atlanta', sport: 'Soccer')
     @competition2 = Competition.create!(name: 'Big Trophy', location: 'Alberta', sport: 'Tennis')
+
+    @team1 = Team.create!(nickname: 'Buzzers', hometown: 'Miami')
+    @team2 = Team.create!(nickname: 'Hawks', hometown: 'Chicago')
+    @team3 = Team.create!(nickname: 'Mustangs', hometown: 'Cleveland')
+
+    @player1 = @team1.players.create!(name: 'Bob', age: 12)
+    @player2 = @team1.players.create!(name: 'Hannah', age: 14)
+    @player3 = @team1.players.create!(name: 'Bert', age: 16)
+    @player4 = @team2.players.create!(name: 'Lou', age: 13)
+    @player5 = @team2.players.create!(name: 'Carrie', age: 15)
+    @player6 = @team2.players.create!(name: 'Sam', age: 14)
+    @player7 = @team3.players.create!(name: 'Tori', age: 14)
+
+    @competition1.teams << [@team1, @team2]
+    @competition2.teams << [@team1, @team3]
   end
 
   describe 'user' do
@@ -15,6 +30,18 @@ RSpec.describe 'Competition Show' do
       expect(page).to have_content(@competition1.sport)
 
       expect(page).to_not have_content(@competition2.name)
+    end
+
+    it 'displays the nicknames and hometowns of teams in competition' do
+      visit "/competitions/#{@competition1.id}"
+
+      expect(page).to have_content(@team1.nickname)
+      expect(page).to have_content(@team1.hometown)
+      expect(page).to have_content(@team2.nickname)
+      expect(page).to have_content(@team2.hometown)
+
+      expect(page).to_not have_content(@team3.nickname)
+      expect(page).to_not have_content(@team3.hometown)
     end
   end
 end
